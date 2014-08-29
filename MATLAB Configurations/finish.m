1f729c73ae81a21f06c82fbf10ea7155a27c1163
Sudip Sinha <sudipsinha.bappa@gmail.com>
2014-08-29 15:40:12 +0200

    + MATLAB Configuration for Logging

---
+++ MATLAB Configurations/finish.m
@@ -0,0 +1,2 @@
+%   Stop logging.
+diary('off');
---
+++ MATLAB Configurations/startup.m
@@ -0,0 +1,8 @@
+%   Start loging all console output in myTextLog.txt
+diary('myTextLog.txt');
+
+%   Add paths of libsvm libraries.
+addpath('~/Documents/MATLAB/Apps/libsvm/');
+
+%   Change working directory to the one specified.
+cd('~/Documents/DecMeg2014/matlab/');
